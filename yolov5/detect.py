b828d468e08e9faea623cd3024b4fed608be236e
Marcus <mgem@itu.dk>
2020-12-12 20:28:38 +0100

    Final Commit

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -113,7 +113,7 @@ def detect(save_img=False):
                     if save_img or view_img:  # Add bbox to image
                         label = '%s %.2f' % (names[int(cls)], conf)
                         center = center_of_rect(xyxy)
-                        print(center[0])
+                        # print(center[0])
                         r.main_movement(trackable=center[0])
                         #  I'm turning off the plotting here as well to improve performance, hopefully.
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
--- yolov5/robot/__init__.py
+++ yolov5/robot/__init__.py
@@ -1,7 +1,7 @@
 from pycreate2 import Create2
 import argparse
-from threading import *
 import time
+from random import randrange
 
 
 class RobotControl:
@@ -10,13 +10,44 @@ class RobotControl:
         self.bot.start()  # Needed to make the robot operate
         self.bot.full()  # We're using safe so that we don't run over edges or some such
         self.sensor = self.bot.get_sensors()
+        self.start = 0.0
+        self.stop = 0.0
+
+        self.turn_around()
 
     def update_sensor(self):
         # print('updating sensor')
         self.sensor = self.bot.get_sensors()
-        print(self.sensor.light_bumper)
-        if any(self.sensor.light_bumper):
-            self.wall_detect()
+        #  print(self.sensor.light_bumper)
+        end = (time.time() - self.start)
+        print(end)
+        print(self.sensor.light_bumper.center_right)
+        # print(self.sensor.light_bumper_right)
+        #  print(self.sensor.light_bumper.center_left)
+        # print(end)
+
+        if self.sensor.light_bumper.center_left > 50 or \
+                self.sensor.light_bumper_center_right > 50 or \
+                self.sensor.light_bumper_front_right > 50 or \
+                self.sensor.light_bumper_front_left > 50:
+            if 2 < end < 5:
+                self.wall_detect()
+            else:
+                self.turn_around()
+
+        # Turn left if you have meet something on the right
+        elif self.sensor.light_bumper_right > 50 or self.sensor.light_bumper_front_right > 50:
+            self.turn_right()
+
+        # Turn right if you have meet something on the left
+        elif self.sensor.light_bumper_left > 50 or self.sensor.light_bumper_front_left > 50:
+            self.turn_left()
+
+        elif end < randrange(start=5, stop=10):
+            self.move_forward()
+
+        else:
+            self.turn_around()
 
     def wall_detect(self):
         print("Wall has been found!")
@@ -29,19 +60,18 @@ class RobotControl:
         time.sleep(12)
         self.turn_around()
         time.sleep(15)
-        self.move_forward()
-        time.sleep(5)
         #  self.bot.drive_direct(r_vel=-25, l_vel=-25)
 
     def turn_around(self):
         self.bot.drive_direct(l_vel=-25, r_vel=25)
+        print("turning around")
 
     def turn_right(self):
-        self.bot.drive_direct(l_vel=10, r_vel=25)
+        self.bot.drive_direct(l_vel=-10, r_vel=25)
         print("turning right")
 
     def turn_left(self):
-        self.bot.drive_direct(l_vel=25, r_vel=10)
+        self.bot.drive_direct(l_vel=25, r_vel=-10)
         print("turning left")
 
     def move_forward(self):
@@ -54,12 +84,15 @@ class RobotControl:
     def main_movement(self, trackable):  # The main movement of the robot
 
         if not any(self.sensor.light_bumper):
+            self.start = time.time()
             if trackable < 213:
                 self.turn_right()
             elif 426 > trackable > 213:
                 self.move_forward()
             elif trackable > 426:
                 self.turn_left()
+        else:
+            self.update_sensor()
 
 
 if __name__ == '__main__':
